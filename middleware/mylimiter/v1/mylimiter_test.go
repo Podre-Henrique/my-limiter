36ce75939f32e3ae89c28c877207e3a87424aa82
Podre-Henrique <pedro.hen.costa34@gmail.com>
2024-03-14 10:40:50 -0300

    Adicioanar uma chamada enqto deleta usuarios inativos

--- middleware/mylimiter/v1/mylimiter_test.go
+++ middleware/mylimiter/v1/mylimiter_test.go
@@ -1,19 +1,30 @@
 package v1
 
 import (
+	"io"
+	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/Podre-Henrique/my-ratelimit/middleware/mylimiter"
 	"github.com/Podre-Henrique/my-ratelimit/middleware/mylimiter/config"
 	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_myLimiter(t *testing.T) {
 	t.Parallel()
 	app := fiber.New()
-	app.Use(mylimiter.New(config.Config{LimitRequest: 2, DurationBan: 1 * time.Minute, DurationInMemory: 30 * time.Second}))
+	app.Use(mylimiter.New(config.Config{LimitRequest: 2, DurationBan: 1 * time.Minute, DurationInMemory: 10 * time.Second}))
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).SendString("Bom dia/tarde/noite")
 	})
+
+	resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/", nil))
+	assert.NoError(t, err)
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+	body, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	assert.Equal(t, "Bom dia/tarde/noite", string(body))
+	// Adicionar uma forma de inserir varios usuarios e fazer uma requisição enquanto estiver deletando os inativos
 }
